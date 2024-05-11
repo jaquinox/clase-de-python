820a3d2fdd4b669f838bcce07f5557b56d24178e
jaquinox <jaquinox123@gmail.com>
2024-05-11 16:47:36 -0400

    variables y aritmetica

---
+++ 01variables.py
@@ -0,0 +1,19 @@
+numero1 =12
+
+entero = 10
+
+decimal = 10.5
+
+
+texto = "holamundo"
+
+
+
+print(entero)
+print(decimal)
+print(texto)
+
+print(type (entero))
+print(type (decimal))
+print(type (texto))
+
---
+++ 02opearitmetica.py
@@ -0,0 +1,24 @@
+
+suma = 2 +2
+resta = 5-10
+
+"""suma +
+resta -
+multiplicacion *
+division /
+modulo %"""
+
+
+
+#print(suma)
+#print(resta)
+
+
+
+numero2 = int(input("Ingrese el primer numero: "))
+numero3 = int(input("Ingrese el segundo numero: "))
+
+suma= numero2 + numero3
+
+
+print(suma)
